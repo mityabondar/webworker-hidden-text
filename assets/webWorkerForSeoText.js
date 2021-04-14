44fe6570d449d9e510c9beab0f54b5a434d2fbd5
Dima Bondar <dimamunsky@gmail.com>
2021-04-14 15:55:33 +0300

    Removed base64 decode

--- assets/webWorkerForSeoText.js
+++ assets/webWorkerForSeoText.js
@@ -1,4 +1,4 @@
 self.addEventListener('message', function(e) {
-    const bannerText = atob(e.data.text)
+    const bannerText = e.data.text
     setTimeout(() => self.postMessage(bannerText), 1000)
 })
