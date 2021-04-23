425d64fc951fa215e198edaf068e19fa8995daa0
Dima Bondar <dimamunsky@gmail.com>
2021-04-23 16:44:11 +0300

    Added check of base64 for input text

--- assets/webWorkerForSeoText.js
+++ assets/webWorkerForSeoText.js
@@ -1,5 +1,12 @@
 self.addEventListener('message', function(e) {
     const inputTexts = e.data.text
-    const outputTexts = inputTexts.map(item => atob(item))
+    const base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/
+
+    function decodeText(text) {
+        const decodedText = base64regex.test(text) ? atob(text) : text
+        return decodedText
+    }
+
+    const outputTexts = inputTexts.map(item => decodeText(item))
     setTimeout(() => self.postMessage(outputTexts), 500)
 })
